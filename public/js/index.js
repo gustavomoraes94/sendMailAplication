48662ea10b2b358fb89fb72eedde0c37eb32398b
Gustavo Moraes <gustavomoraes1994@gmail.com>
2023-08-31 14:08:20 -0300

    debbug processo de envio

--- public/js/index.js
+++ public/js/index.js
@@ -151,7 +151,7 @@ function showPass(passo){
     document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
 
-function vlrTotal(obj){
+function setVlrTotal(obj){
     const values = Object.values(obj);
 
     const sum = values.reduce((a ,b ) => {
@@ -161,10 +161,41 @@ function vlrTotal(obj){
     return sum;
 }
 
-function vlrTotalEventual(){
-    const sum = despesasEventuais.despVlr1 + despesasEventuais.despVlr2 + despesasEventuais.despVlr3 + despesasEventuais.despVlr4 + despesasEventuais.despVlr5;
-    
-    return sum;
+function setVlrTotalMoradia(obj,moradores){
+    const value = Object.values(obj);
+
+    const sum = value.reduce((a,b) =>{
+        return a+b;
+    },0);
+
+    if(moradores != 0){
+        return sum/moradores;
+
+    }else{
+        return sum    
+    }
+
+}
+
+
+function getValue(name){
+    const value = document.getElementById(`${name}`).value;
+
+    if (value == NaN || value == null || value == ''){
+        return 0;
+    }else{
+        return parseFloat(value);
+    }
+}
+
+function getName(name){
+    const value = document.getElementById(`${name}`).value;
+    return value;
+}
+
+function rondValue(value){
+    const newValue = parseFloat(value).toFixed(2);
+    return `${newValue.toString().replace('.',',')} R$`; 
 }
 
 function sendMail(){
@@ -181,107 +212,110 @@ function sendMail(){
     personalData.email = document.getElementById('email').value;
     personalData.tel = document.getElementById('telefone').value;
 
-    despesasMensais.planSaude = parseFloat(document.getElementById('planSaude').value); 
-    despesasMensais.ingles = parseFloat(document.getElementById('ingles').value);
-    despesasMensais.psicologo = parseFloat(document.getElementById('psicologo').value);
-    despesasMensais.atvExtra = parseFloat(document.getElementById('atvExtra').value);
-    despesasMensais.atvExtra2 = parseFloat(document.getElementById('atvExtra2').value);
-    despesasMensais.farmaciaHigiene = parseFloat(document.getElementById('farmacia').value);
-    despesasMensais.lazer = parseFloat(document.getElementById('lazer').value);
-    despesasMensais.vestimenta = parseFloat(document.getElementById('vestimenta').value);
-    despesasMensais.merenda = parseFloat(document.getElementById('merenda').value);
-    despesasMensais.deslocamento = parseFloat(document.getElementById('deslocamento').value);
-    despesasMensais.mensalidadeEscolar = parseFloat(document.getElementById('mensalidadeEscolar').value);
-    vlrTotalMensal = parseFloat(vlrTotal(despesasMensais)).toFixed(2);
+    despesasMensais.planSaude = getValue('planSaude'); 
+    despesasMensais.ingles = getValue('ingles');
+    despesasMensais.psicologo = getValue('psicologo');
+    despesasMensais.atvExtra = getValue('atvExtra');
+    despesasMensais.atvExtra2 = getValue('atvExtra2');
+    despesasMensais.farmaciaHigiene = getValue('farmacia');
+    despesasMensais.lazer = getValue('lazer');
+    despesasMensais.vestimenta = getValue('vestimenta');
+    despesasMensais.merenda = getValue('merenda');
+    despesasMensais.deslocamento = getValue('deslocamento');
+    despesasMensais.mensalidadeEscolar = getValue('mensalidadeEscolar');
+    vlrTotalMensal = setVlrTotal(despesasMensais) ;
 
-    numMoradores = parseInt(document.getElementById('moradores').value);
-    moradia.empregada = parseFloat(document.getElementById('empregada').value);
-    moradia.eSocial = parseFloat(document.getElementById('e-social').value);
-    moradia.passagem = parseFloat(document.getElementById('passagem').value);
-    moradia.condominio = parseFloat(document.getElementById('condiminio').value);
-    moradia.iptu = parseFloat(document.getElementById('iptu').value);
-    moradia.luz = parseFloat(document.getElementById('luz').value);
-    moradia.agua = parseFloat(document.getElementById('agua').value);
-    moradia.gas = parseFloat(document.getElementById('gas').value);
-    moradia.transEscolar = parseFloat(document.getElementById('transporteEscolar').value);
-    moradia.internet = parseFloat(document.getElementById('internet').value);
-    moradia.streaming = parseFloat(document.getElementById('streaming').value);
-    moradia.tvAssinatura = parseFloat(document.getElementById('tvAssinatura').value);
-    moradia.tel = parseFloat(document.getElementById('telVlr').value);
-    moradia.aluguel = parseFloat(document.getElementById('aluguel').value);
-    vlrTotalMoradia = parseFloat(vlrTotal(moradia)/numMoradores).toFixed(2);
+    numMoradores = getValue('moradores');
+    moradia.empregada = getValue('empregada');
+    moradia.eSocial = getValue('e-social');
+    moradia.passagem = getValue('passagem');
+    moradia.condominio = getValue('condiminio');
+    moradia.iptu = getValue('iptu');
+    moradia.luz = getValue('luz');
+    moradia.agua = getValue('agua');
+    moradia.gas = getValue('gas');
+    moradia.transEscolar = getValue('transporteEscolar');
+    moradia.internet = getValue('internet');
+    moradia.streaming = getValue('streaming');
+    moradia.tvAssinatura = getValue('tvAssinatura');
+    moradia.tel = getValue('telVlr');
+    moradia.aluguel = getValue('aluguel');
+    vlrTotalMoradia = setVlrTotalMoradia(moradia,numMoradores);
     
-    despesasAnuais.materialEscolar = parseFloat(document.getElementById('materialScolar').value);
-    despesasAnuais.uniforme = parseFloat(document.getElementById('uniforme').value);
-    despesasAnuais.dentista = parseFloat(document.getElementById('dentista').value);
-    despesasAnuais.materialDidatico = parseFloat(document.getElementById('materialDidatico').value);
-    vlrTotalAnual = parseFloat(vlrTotal(despesasAnuais)).toFixed(2);
+    despesasAnuais.materialEscolar = getValue('materialScolar');
+    despesasAnuais.uniforme = getValue('uniforme');
+    despesasAnuais.dentista = getValue('dentista');
+    despesasAnuais.materialDidatico = getValue('materialDidatico');
+    vlrTotalAnual = setVlrTotal(despesasAnuais);
     
-    nomesDespesasEventuais.despName1 = document.getElementById('nomeExtra1').value;
-    nomesDespesasEventuais.despName2 = document.getElementById('nomeExtra2').value;
-    nomesDespesasEventuais.despName3 = document.getElementById('nomeExtra3').value;
-    nomesDespesasEventuais.despName4 = document.getElementById('nomeExtra4').value;
-    nomesDespesasEventuais.despName5 = document.getElementById('nomeExtra5').value;
-    valoresDespesasEventuais.despVlr1 = parseFloat(document.getElementById('vlrDespesa1').value);
-    valoresDespesasEventuais.despVlr2 = parseFloat(document.getElementById('vlrDespesa2').value);
-    valoresDespesasEventuais.despVlr3 = parseFloat(document.getElementById('vlrDespesa3').value);
-    valoresDespesasEventuais.despVlr4 = parseFloat(document.getElementById('vlrDespesa4').value);
-    valoresDespesasEventuais.despVlr5 = parseFloat(document.getElementById('vlrDespesa5').value);
-    vlrTotalEventual = parseFloat(vlrTotal(valoresDespesasEventuais)).toFixed(2);
+    nomesDespesasEventuais.despName1 = getName('nomeExtra1');
+    nomesDespesasEventuais.despName2 = getName('nomeExtra2');
+    nomesDespesasEventuais.despName3 = getName('nomeExtra3');
+    nomesDespesasEventuais.despName4 = getName('nomeExtra4');
+    nomesDespesasEventuais.despName5 = getName('nomeExtra5');
+    valoresDespesasEventuais.despVlr1 = getValue('vlrDespesa1');
+    valoresDespesasEventuais.despVlr2 = getValue('vlrDespesa2');
+    valoresDespesasEventuais.despVlr3 = getValue('vlrDespesa3');
+    valoresDespesasEventuais.despVlr4 = getValue('vlrDespesa4');
+    valoresDespesasEventuais.despVlr5 = getValue('vlrDespesa5');
+    vlrTotalEventual = setVlrTotal(valoresDespesasEventuais);
 
     let params ={
         nome: personalData.nome,
         email: personalData.email,
         telefone: personalData.tel,
-        planSaude: despesasMensais.planSaude,
-        ingles: despesasMensais.ingles,
-        psicologo: despesasMensais.psicologo,
-        atvExtra: despesasMensais.atvExtra,
-        atvExtra2: despesasMensais.atvExtra2,
-        farmaciaHigiene: despesasMensais.farmaciaHigiene,
-        lazer: despesasMensais.lazer,
-        vestimenta: despesasMensais.vestimenta,
-        merenda: despesasMensais.merenda,
-        deslocamento: despesasMensais.deslocamento,
-        mensalidadeEscolar: despesasMensais.mensalidadeEscolar,
-        vlrTotalMensal: vlrTotalMensal,
+        planSaude: rondValue(despesasMensais.planSaude),
+        ingles: rondValue(despesasMensais.ingles),
+        psicologo: rondValue(despesasMensais.psicologo),
+        atvExtra: rondValue(despesasMensais.atvExtra),
+        atvExtra2: rondValue(despesasMensais.atvExtra2),
+        farmaciaHigiene: rondValue(despesasMensais.farmaciaHigiene),
+        lazer: rondValue(despesasMensais.lazer),
+        vestimenta: rondValue(despesasMensais.vestimenta),
+        merenda: rondValue(despesasMensais.merenda),
+        deslocamento: rondValue(despesasMensais.deslocamento),
+        mensalidadeEscolar: rondValue(despesasMensais.mensalidadeEscolar),
+        vlrTotalMensal: rondValue(vlrTotalMensal),
 
         numMoradores: numMoradores,
 
-        empregada: moradia.empregada,
-        eSocial: moradia.eSocial,
-        passagem: moradia.passagem,
-        condominio: moradia.condominio,
-        iptu: moradia.iptu,
-        luz: moradia.luz,
-        agua: moradia.agua,
-        gas: moradia.gas,
-        transEscolar: moradia.transEscolar,
-        internet: moradia.internet,
-        streaming: moradia.streaming,
-        tvAssinatura: moradia.tvAssinatura,
-        tel: moradia.tel,
-        aluguel: moradia.aluguel,
-        vlrTotalMoradia: moradia.vlrTotal,
-        materialEscolar: despesasAnuais.materialEscolar,
-        uniforme: despesasAnuais.uniforme,
-        dentista: despesasAnuais.dentista,
-        materialDidatico: despesasAnuais.materialDidatico,
-        vlrTotalAnual: vlrTotalAnual,
+        empregada: rondValue(moradia.empregada),
+        eSocial: rondValue(moradia.eSocial),
+        passagem: rondValue(moradia.passagem),
+        condominio: rondValue(moradia.condominio),
+        iptu: rondValue(moradia.iptu),
+        luz: rondValue(moradia.luz),
+        agua: rondValue(moradia.agua),
+        gas: rondValue(moradia.gas),
+        transEscolar: rondValue(moradia.transEscolar),
+        internet: rondValue(moradia.internet),
+        streaming: rondValue(moradia.streaming),
+        tvAssinatura: rondValue(moradia.tvAssinatura),
+        tel: rondValue(moradia.tel),
+        aluguel: rondValue(moradia.aluguel),
+        vlrTotalMoradia: rondValue(vlrTotalMoradia),
+
+        materialEscolar: rondValue(despesasAnuais.materialEscolar),
+        uniforme: rondValue(despesasAnuais.uniforme),
+        dentista: rondValue(despesasAnuais.dentista),
+        materialDidatico: rondValue(despesasAnuais.materialDidatico),
+        vlrTotalAnual: rondValue(vlrTotalAnual),
 
         despName1: nomesDespesasEventuais.despName1,
         despName2: nomesDespesasEventuais.despName2,
         despName3: nomesDespesasEventuais.despName3,
         despName4: nomesDespesasEventuais.despName4,
         despName5: nomesDespesasEventuais.despName5,
-        despVlr1: valoresDespesasEventuais.despVlr1,
-        despVlr2: valoresDespesasEventuais.despVlr2,
-        despVlr3: valoresDespesasEventuais.despVlr3,
-        despVlr4: valoresDespesasEventuais.despVlr4,
-        despVlr5: valoresDespesasEventuais.despVlr5,
-        vlrTotalEventual: vlrTotalEventual
+        despVlr1: rondValue(valoresDespesasEventuais.despVlr1),
+        despVlr2: rondValue(valoresDespesasEventuais.despVlr2),
+        despVlr3: rondValue(valoresDespesasEventuais.despVlr3),
+        despVlr4: rondValue(valoresDespesasEventuais.despVlr4),
+        despVlr5: rondValue(valoresDespesasEventuais.despVlr5),
+        vlrTotalEventual: rondValue(vlrTotalEventual)
 
     }
+
+    // console.log(params);
 
     emailjs
         .send(serviceID,templateID, params)
